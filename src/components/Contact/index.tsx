4225cadd7ee6b9dc8aab55b64bda1990bec124f3
Imran <imran.qadeer@nexigroup.com>
2025-05-02 14:54:00 +0200

    updated

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -7,14 +7,14 @@ const Contact = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
             <div
-              className="mb-12 rounded-xs bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              className="shadow-three dark:bg-gray-dark mb-12 rounded-xs bg-white px-8 py-11 sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
               "
             >
-              <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+              <h2 className="mb-3 text-2xl font-bold text-black sm:text-3xl lg:text-2xl xl:text-3xl dark:text-white">
                 Need Help? Open a Ticket
               </h2>
-              <p className="mb-12 text-base font-medium text-body-color">
+              <p className="text-body-color mb-12 text-base font-medium">
                 Our support team will get back to you ASAP via email.
               </p>
               <form>
@@ -22,15 +22,108 @@ const Contact = () => {
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
                       <label
+                        htmlFor="gender"
+                        className="text-dark mb-3 block text-sm font-medium dark:text-white"
+                      >
+                        Select Gender
+                      </label>
+                      <select
+                        id="gender"
+                        className="border-stroke text-body-color focus:border-primary dark:text-body-color-dark dark:shadow-two dark:focus:border-primary w-full rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base outline-hidden dark:border-transparent dark:bg-[#2C303B] dark:focus:shadow-none"
+                      >
+                        <option value="">Select</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="relative mb-8">
+                      <label
+                        htmlFor="gender"
+                        className="text-dark mb-3 block text-sm font-medium dark:text-white"
+                      >
+                        Modified select
+                      </label>
+                      <select
+                        id="gender"
+                        className="border-stroke text-body-color focus:border-primary dark:text-body-color-dark dark:shadow-two dark:focus:border-primary w-full rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base font-semibold outline-hidden dark:border-transparent dark:bg-[#2C303B] dark:focus:shadow-none"
+                      >
+                        <option
+                          className="py-2 font-bold hover:bg-gray-100 dark:hover:bg-gray-700"
+                          value=""
+                        >
+                          Select
+                        </option>
+                        <option
+                          className="py-2 font-bold hover:bg-gray-100 dark:hover:bg-gray-700"
+                          value="male"
+                        >
+                          Male
+                        </option>
+                        <option
+                          className="py-2 font-bold hover:bg-gray-100 dark:hover:bg-gray-700"
+                          value="female"
+                        >
+                          Female
+                        </option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="mt-12 w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label className="text-dark flex items-center space-x-3 text-sm font-medium dark:text-white">
+                        <input
+                          type="checkbox"
+                          className="border-stroke text-primary focus:ring-primary dark:checked:bg-primary h-8 w-8 rounded bg-[#f8f8f8] focus:ring-2 dark:border-gray-600 dark:bg-[#2C303B]"
+                        />
+                        <span>Subscribe to newsletter</span>
+                      </label>
+                    </div>
+                  </div>
+
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <p className="text-dark mb-3 block text-sm font-medium dark:text-white">
+                        Choose Plan
+                      </p>
+                      <div className="flex items-center space-x-6">
+                        <label className="text-dark flex items-center space-x-2 text-sm dark:text-white">
+                          <input
+                            type="radio"
+                            name="plan"
+                            value="basic"
+                            className="text-primary focus:ring-primary h-6 w-6 dark:border-gray-600 dark:bg-[#2C303B]"
+                          />
+                          <span>Basic</span>
+                        </label>
+                        <label className="text-dark flex items-center space-x-2 text-sm dark:text-white">
+                          <input
+                            type="radio"
+                            name="plan"
+                            value="premium"
+                            className="text-primary focus:ring-primary h-6 w-6 dark:border-gray-600 dark:bg-[#2C303B]"
+                          />
+                          <span>Premium</span>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
                         htmlFor="name"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                        className="text-dark mb-3 block text-sm font-medium dark:text-white"
                       >
                         Your Name
                       </label>
                       <input
                         type="text"
                         placeholder="Enter your name"
-                        className="border-stroke w-full rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-hidden focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                        className="border-stroke text-body-color focus:border-primary dark:text-body-color-dark dark:shadow-two dark:focus:border-primary w-full rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base outline-hidden dark:border-transparent dark:bg-[#2C303B] dark:focus:shadow-none"
                       />
                     </div>
                   </div>
@@ -38,14 +131,14 @@ const Contact = () => {
                     <div className="mb-8">
                       <label
                         htmlFor="email"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                        className="text-dark mb-3 block text-sm font-medium dark:text-white"
                       >
                         Your Email
                       </label>
                       <input
                         type="email"
                         placeholder="Enter your email"
-                        className="border-stroke w-full rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-hidden focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                        className="border-stroke text-body-color focus:border-primary dark:text-body-color-dark dark:shadow-two dark:focus:border-primary w-full rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base outline-hidden dark:border-transparent dark:bg-[#2C303B] dark:focus:shadow-none"
                       />
                     </div>
                   </div>
@@ -53,7 +146,7 @@ const Contact = () => {
                     <div className="mb-8">
                       <label
                         htmlFor="message"
-                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                        className="text-dark mb-3 block text-sm font-medium dark:text-white"
                       >
                         Your Message
                       </label>
@@ -61,12 +154,12 @@ const Contact = () => {
                         name="message"
                         rows={5}
                         placeholder="Enter your Message"
-                        className="border-stroke w-full resize-none rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-hidden focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                        className="border-stroke text-body-color focus:border-primary dark:text-body-color-dark dark:shadow-two dark:focus:border-primary w-full resize-none rounded-xs border bg-[#f8f8f8] px-6 py-3 text-base outline-hidden dark:border-transparent dark:bg-[#2C303B] dark:focus:shadow-none"
                       ></textarea>
                     </div>
                   </div>
                   <div className="w-full px-4">
-                    <button className="rounded-xs bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                    <button className="bg-primary shadow-submit hover:bg-primary/90 dark:shadow-submit-dark rounded-xs px-9 py-4 text-base font-medium text-white duration-300">
                       Submit Ticket
                     </button>
                   </div>
